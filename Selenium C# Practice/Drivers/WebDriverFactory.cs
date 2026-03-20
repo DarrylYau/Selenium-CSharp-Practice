1eeed55e369d43996947659e8a605772a6bbf842
Darryl <wongkwangyau@yahoo.com.my>
2026-03-20 13:57:48 +0800

    feat: Update WebDriverFavtory.cs to support headless Chrome for GitHub Actions CI

--- Selenium C# Practice/Drivers/WebDriverFactory.cs
+++ Selenium C# Practice/Drivers/WebDriverFactory.cs
@@ -8,6 +8,11 @@ public class WebDriverFactory
 {
     public static IWebDriver CreateDriver()
     {
-        return new ChromeDriver();
+        var options = new ChromeOptions();
+        options.AddArgument("--headless");// Run Chrome in headless mode for testing without opening a browser window")
+        options.AddArgument("--no-sandbox"); // Bypass OS security model, required for running Chrome in headless mode as root user
+        options.AddArgument("--disable-dev-shm-usage"); // Overcome limited resource problems in Docker containers
+
+        return new ChromeDriver(options);
     }
 }
